e58a72506a5a339db30c762484154993b82af3a6
Priyanshii <Priyanshik222@gmail.com>
2023-03-28 18:46:03 +0530

    first commit

---
+++ app/src/main/java/com/priyu/intnent/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.priyu.intnent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    public Button btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        btn2 = findViewById(R.id.button1);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
